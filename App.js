3b9f5cb4e406eccfed8289564974c4082af109e3
josh-code <fernandes.developer@gmail.com>
2021-03-30 20:08:40 +0530

    TrasferdToNewPC

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import AppPicker from "./app/components/AppPicker";
 import AppScreen from "./app/components/AppScreen";
 import AppTextInput from "./app/components/AppTextInput";
 import LoginScreen from "./app/screens/LoginScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <LoginScreen />;
+  return <MessagesScreen />;
 }
